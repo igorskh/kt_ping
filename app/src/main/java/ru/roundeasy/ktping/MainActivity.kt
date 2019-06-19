1a6324b183050c4bc4cf645cfdb4334a20a16356
Igor Kim <igor.skh@gmail.com>
2019-06-19 14:53:12 +0200

    Format datetime in readable format Use queue for ping results Add count parameter Use process builder addresses #1

--- app/src/main/java/ru/roundeasy/ktping/MainActivity.kt
+++ app/src/main/java/ru/roundeasy/ktping/MainActivity.kt
@@ -8,11 +8,13 @@ import android.widget.TableRow
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.lang.ref.WeakReference
 import java.util.regex.Matcher
 import java.util.regex.Pattern
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToLong
+
 
 /**
  * A Main Activity class
@@ -20,13 +22,15 @@ import java.util.regex.Pattern
  * This class contains the main logic of the app
  */
 class MainActivity : AppCompatActivity() {
+    private val pingRegex = "(?:\\[(?<ts>[0-9.]+)] )?(?<size>[0-9]+) bytes from (?<ip>[0-9.]+): icmp_seq=(?<seq>[0-9]+) ttl=(?<ttl>[0-9]+)(?: time=(?<rtt>[0-9.]+) (?<rttmetric>\\w+))?"
     private val outerClass = WeakReference<MainActivity>(this)
     private var mHandlerThread = MyHandler(outerClass)
 
     private var mThread: Thread? = null
     private var isThreadRunning = false
-    private var lastPingString = ""
     private var errorMessage = ""
+
+    val queue: Queue<String> = ArrayDeque<String>()
 
     companion object {
         private const val START = 100
@@ -47,14 +51,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun parsePingString(s: String): Matcher {
         val re = Pattern.compile(
-            "\\[([0-9.]+)] ([0-9]+) bytes from ([0-9.]+): icmp_seq=([0-9]+) ttl=([0-9]+) time=([0-9.]+) (\\w+)",
+            pingRegex,
             Pattern.CASE_INSENSITIVE.or(Pattern.DOTALL))
         return re.matcher(s)
 
     }
 
     private fun updateText() {
-        val res = parsePingString(lastPingString)
+        if (queue.size == 0) {
+            return
+        }
+
+        val res = parsePingString(queue.remove())
 
         if (!res.find()) {
             return
@@ -69,12 +77,26 @@ class MainActivity : AppCompatActivity() {
         val tvSeqN = rowView.findViewById(R.id.tvSeqN) as? TextView
         val tvTtl = rowView.findViewById(R.id.tvTtl) as? TextView
         val tvRtt = rowView.findViewById(R.id.tvRtt) as? TextView
-        tvTimestamp?.text = res.group(1)
+
+        var dateTimeStr = res.group(1)
+        try {
+            val sdf = SimpleDateFormat(getString(R.string.datetime_format), Locale.US)
+            val netDate = Date((dateTimeStr.toDouble()*1000).roundToLong())
+            dateTimeStr = sdf.format(netDate)
+        } finally {
+            tvTimestamp?.text = dateTimeStr
+        }
+
         tvSize?.text = res.group(2)
         tvTarget?.text =res.group(3)
         tvSeqN?.text = res.group(4)
         tvTtl?.text = res.group(5)
-        tvRtt?.text = resources.getString(R.string.format_rtt, res.group(6), res.group(7))
+
+        val rttConcat = if (res.group(6) != null)
+                resources.getString(R.string.format_rtt, res.group(6), res.group(7))
+            else resources.getString(R.string.value_na)
+
+        tvRtt?.text = rttConcat
         tableLayout.addView(rowView, 1)
     }
 
@@ -83,6 +105,7 @@ class MainActivity : AppCompatActivity() {
             val serverAddr = textServer.text.toString()
             val size = textSize.text.toString()
             val interval = textInterval.text.toString()
+            val count = textCount.text.toString()
 
             val cmd = mutableListOf("ping", "-D")
             if (size != "") {
@@ -91,36 +114,42 @@ class MainActivity : AppCompatActivity() {
             if (interval != "") {
                 cmd.addAll(arrayOf("-i", interval))
             }
+            if (count != "") {
+                cmd.addAll(arrayOf("-c", count))
+            }
             cmd.add(serverAddr)
 
-            val process = Runtime.getRuntime().exec(cmd.toTypedArray())
+            val builder = ProcessBuilder()
+            builder.command(cmd)
 
-            val stdInput = BufferedReader(InputStreamReader(process.inputStream))
+            val process = builder.start()
+            val stdInput = process.inputStream.bufferedReader()
 
             val messageStart = Message()
             messageStart.what = START
             mHandlerThread.sendMessage(messageStart)
 
-            for (l in stdInput.lines()) {
-                if (!isThreadRunning) {
+            while (isThreadRunning) {
+                val currentStr = try {
+                    stdInput.readLine()
+                } catch (e: IllegalStateException) {
                     break
-                }
+                } ?: break
 
-                try {
-                    Thread.sleep(100)
-                } catch (ex: InterruptedException) {
-                    ex.printStackTrace()
-                }
+                pingRegex.toRegex().find(currentStr) ?: continue
 
-                lastPingString = l
+                queue.add(currentStr)
 
                 val messagePing = Message()
                 messagePing.what = PING
                 mHandlerThread.sendMessage(messagePing)
             }
             if (isThreadRunning) {
-                val errOutput = BufferedReader(InputStreamReader(process.errorStream))
-                errorMessage = errOutput.readLine()
+                errorMessage = try {
+                    process.errorStream.bufferedReader().readLine()
+                } catch (e: IllegalStateException) {
+                    ""
+                }
             }
 
             val messageStop = Message()
